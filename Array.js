b995c558cd46b6da4ebc61feacff9ceda4340fe8
Shariea Reza Nabil <nabilreza183@gmail.com>
2026-01-28 01:35:24 +0600

    practice loop

---
+++ Array.js
@@ -0,0 +1,39 @@
+
+// const fruite = ['Mango', "Papa", 'Orange', 'Banana', 'Apple'];
+// console.log(fruite[3])
+
+
+// const tourist = ['Dubai', 'Rome', 'Paris'];
+// tourist.push('Tokyo');
+// tourist.push('Madina', 'Makkha');
+// tourist.pop();
+// console.log(tourist)
+
+
+// const books = ['Animal', 'JavaScript', 'Python', 'Hablu', 'Momotaj'];
+// const check = books.includes('JavaScript');
+
+// if (check) {
+//     console.log('Yes! this is here')
+// } else {
+//     console.log('No. Bad Luck. You have no money');
+// }
+
+
+
+
+// const food = ['Rice', "Cucumber"];
+
+// if (Array.isArray(food)) {
+//     console.log('Yes! this is a Array')
+// }else {
+//     console.log('No! this is not a Array')
+// }
+
+
+
+
+// const array01 = ['Paglu', 'Chaglu', 'Modon']
+// const array02 = ['Pagol', 'Chagol', 'Alu'];
+// const connect = array01.concat(array02);
+// console.log(array01, array02, connect);
---
+++ Loop.js
@@ -0,0 +1,187 @@
+// const number = [12, 34, 56, 77, 89];
+// for (const num of number) {
+//     console.log(num);
+// }
+
+
+
+// let num = 0;
+// while (num < 5) {
+//     console.log('Hello');
+//     num++;
+// }
+
+
+
+// let num = 0;
+// for (let num = 0; num < 5; num++) {
+//     console.log('Gelo', num);
+// }
+
+
+
+
+
+//.................for loop.....................................\\
+
+// for (let num = 1; num <= 60; num++) {
+//     console.log('I will invest at least 6 hrs every single day for next 60 days!', num);
+// }
+
+
+// for (let i = 61; i <= 100; i++) {
+//     if (i % 2 === 1) {
+//         console.log(i)
+//     }
+// }
+
+
+// for (let a = 78; a <= 98; a++) {
+//     if (a % 2 !== 1) {
+//         console.log(a);
+//     }
+// }
+
+// let sum = 0;
+// for (let b = 91; b <= 129; b++) {
+//     if(b % 2 !== 0) {
+//         sum = sum + b;
+//     }
+// }
+// console.log(sum);
+
+
+// let sum = 0;
+// for (let c = 51; c <= 85; c++) {
+//     if (c % 2 === 0) {
+//         sum = sum + c;
+//     }
+// }
+// console.log(sum);
+
+
+// const num = 9;
+// for (let d = 1; d <= 10; d++) {
+//     let multiplication = `${num} x ${d} = ${num * d}`; 
+//     console.log(multiplication);
+// };
+
+
+
+// for (let e = 81; e >= 65; e--) {
+//     console.log(e);
+// }
+
+
+
+
+
+
+
+
+
+
+//..........................While loop.........................................\\
+
+
+// let a = 1;
+// while(a <= 60) {
+//     console.log('I will invest at least 6 hrs every single day for next 60 days!', a);
+//     a++;
+// }
+
+// let b = 61;
+// while(b <= 100) {
+//     if ( b % 2 === 1) {
+//         console.log(b);
+//     }
+//     b++;
+// }
+
+// let c = 78;
+// while(c <= 98) {
+//     if (c % 2 === 0) {
+//         console.log(c)
+//     }
+//     c++;
+// }
+
+
+// let d = 206;
+// let sum = 0;
+// while(d <= 311) {
+//     if (d % 2 === 0) {
+//         sum = sum + d;
+//     }
+//     d++;
+// }
+// console.log(sum);
+
+
+// let f = 81;
+// let sum = 0;
+// while(f <= 131) {
+//     if (f % 2 === 1) {
+//         sum = sum + f;
+//     }
+//     f++;
+// }
+// console.log(sum);
+
+// const num = 5;
+// let g = 1;
+// while (g <= 10) {
+//     let multiplication = `${num} x ${g} = ${num * g}`
+//     console.log(multiplication);
+//     g++;
+// }
+
+
+// let h = 21;
+// while(h >= 15) {
+//     console.log(h);
+//     h--;
+// }
+
+
+
+// for (let i = 1; i <= 200; i++) {
+//     if (i === 100) {
+//         break;
+//     }
+//     console.log(i)  
+// }
+
+
+// let num = 0;
+// let k = 1;
+// while(true) {
+//     num = num + k;
+//     console.log(num, k); 
+//     if (num >= 100) {
+//         break;
+//     }
+//     k++;
+// }
+
+
+// for (let i = 1; i <= 40; i++) {
+//     if (i % 2 === 1) {
+//         continue;
+//     }
+//     console.log(i) 
+// }
+
+
+
+
+// for (let b = 55; b <= 85; b++) {
+//     if (b % 2 === 1) {
+//         if (b % 5 === 0) {
+//             continue
+//         } else {
+//             console.log(b);
+//         }
+//     }
+// }
+
