d28a554f9bfb6e2c05b6ccecff4dfa9cc74d4ac2
Shariea Reza Nabil <nabilreza183@gmail.com>
2026-02-01 19:38:18 +0600

    compelete function task

---
+++ function.js
@@ -0,0 +1,61 @@
+// function multiplication(a, b, c, d) {
+//     return a * b * c * d;
+// };
+// const multi = multiplication(3, 4, 5, 6);
+// console.log(multi);
+
+
+// function isOddOrEven(x) {
+//     if (x % 2 === 1) {
+//         return x * 2;
+//     } else {
+//         return x / 2;
+//     }
+// };
+// const result = isOddOrEven(8);
+// console.log(result);
+
+
+
+
+// function make_avg(integers, size_of_array) {
+//     let sum = 0;
+//     for(const int of integers) {
+//         sum = sum + int;
+//     }
+//     return sum / size_of_array;
+// }
+
+// const array = [1, 2, 3, 4, 5, 6, 7, 8];
+// const result = make_avg(array, 8);
+// console.log(result);
+
+
+
+
+
+
+// function count_zero(binary_string) {
+//     let zero = [];
+//     for (const countZero of binary_string) {
+//         if ( countZero === '0') {
+//             zero.push(countZero);
+//         }
+//     }
+//     return zero.length;
+// }
+
+// const count = count_zero('0011001010110');
+// console.log(count);
+
+
+//   function odd_even(integrs) {
+//     if (integrs % 2 === 0) { 
+//         return  'Even';
+//     } else {
+//         return 'Odd';
+//     }
+//   }
+
+//   const int = odd_even(3);
+//   console.log(int);
